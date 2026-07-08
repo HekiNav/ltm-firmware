a690efae2e91816d701480c71015dd57a8c93b67
Unto <developer.hekinav@gmail.com>
2026-07-08 17:50:35 +0300

    update workflow with r2 bucket

--- website/3d_viewer/main.js
+++ website/3d_viewer/main.js
@@ -1,5 +1,8 @@
 const board_id = new URLSearchParams(window.location.search).get("board_id") || "hki-ltm";
-document.querySelector("model-viewer").setAttribute("src",`/assets/${board_id}.glb`)
+
+const model_path = (window.location.hostname == "ltm.hekinav.dev") ? "" : "/assets/"
+
+document.querySelector("model-viewer").setAttribute("src",`${model_path}${board_id}.glb`)
 
 document.querySelectorAll(".board-select").forEach(element => {
     if (element.id == board_id) element.hidden = true
